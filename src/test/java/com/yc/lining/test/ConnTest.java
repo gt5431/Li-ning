4a4f73165658d9438e78fdc24b553eacfd1a64c4
gt5431 <gt5431@foxmail.com>
2016-05-24 00:17:14 +0800

    test 修改了

--- src/test/java/com/yc/lining/test/ConnTest.java
+++ src/test/java/com/yc/lining/test/ConnTest.java
@@ -36,7 +36,7 @@ public class ConnTest {
 	@Test
 	public void testConn2() {
 		conn = sqlSessionFactory.openSession(true).getConnection();
-		assertNotNull("连接失败!!",conn);
+		assertNotNull("连接失败ConnectFail!!",conn);
 	}
 
 
