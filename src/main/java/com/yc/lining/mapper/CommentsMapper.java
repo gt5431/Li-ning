22f78c9dcf0e67ebd7fb98bc7a3b343b3ba73db8
gt5431 <gt5431@foxmail.com>
2016-05-27 13:17:55 +0800

    修改完成

--- src/main/java/com/yc/lining/mapper/CommentsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.yc.lining.mapper;
-
-import com.yc.lining.entity.Comments;
-
-public interface CommentsMapper {
-    int insert(Comments record);
-
-    int insertSelective(Comments record);
-}
